44848b5c8bf77e89299545b5171bf938f7f43b45
Stephen Wright <scw248@gmail.com>
2019-02-19 15:46:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,17 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+<<<<<<< HEAD
 
   self.column_names.each do |col_name|    
     attr_accessor col_name.to_sym  
     end
       
+=======
+  
+  self.column_names.each do |col_name|    
+  attr_accessor col_name.to_sym  
+  end
+  
+>>>>>>> 115e517d546df03615c0c83dd4ae8e645ead5f0d
 end
